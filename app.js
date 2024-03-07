1f94f33d6a4766de85c72cca74720b450cd4fde2
Mdsaqiboffical <154467672+Mdsaqiboffical@users.noreply.github.com>
2024-03-07 16:46:55 +0500

    table done

---
+++ app.js
@@ -0,0 +1,11 @@
+var newTable1 = "Table 2"
+console.log(newTable1)
+for (var i = 1; i <= 10; i++) {
+    console.log( " 2 x " + i + " = " + (2 * i));
+}
+var newTable2 = "Table 8"
+console.log(newTable2)
+
+for (var i = 1; i <= 10; i++) {
+    console.log( " 8 x " + i + " = " + (8 * i));
+}
